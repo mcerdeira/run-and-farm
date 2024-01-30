55f7e0b3b2f2eb553d7e7aeadbf9843ee45a0ea6
martin_cerdeira <Martin.Cerdeira@comafi.com.ar>
2024-01-30 13:49:19 -0300

    Movimiento inicial

--- src/scenes/player.gd
+++ src/scenes/player.gd
@@ -1,11 +1,32 @@
 extends Node2D
+var walk_speed = 100
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$sprite.play("idle")
+	$eyes.play("idle")
 
+func _physics_process(delta):
+	var left = Input.is_action_pressed("left")
+	var right = Input.is_action_pressed("right")
+	var up = Input.is_action_pressed("up")
+	var down = Input.is_action_pressed("down")
+	var moving = left or right or up or down
+	
+	
+	if left:
+		position.x -= walk_speed * delta
+		scale.x = -1
+	elif right:
+		position.x += walk_speed * delta
+		scale.x = 1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	if up:
+		position.y -= walk_speed * delta
+	elif down:
+		position.y += walk_speed * delta
+		
+	if moving:
+		$sprite.animation = "run"
+	else:
+		$sprite.animation = "idle"
+	
