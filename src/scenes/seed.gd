add25a77ca2d87d2962f670a52d7b5b8bebacc27
martin_cerdeira <Martin.Cerdeira@comafi.com.ar>
2024-03-26 09:53:33 -0300

    Cambiar terreno, absorver semillas

--- src/scenes/seed.gd
+++ src/scenes/seed.gd
@@ -3,17 +3,24 @@ var SPEED = 10.0
 var target = null
 var taken = false
 var tier = Global.SeedTiers.D
+var absorved = false
+var player = null
+var speed = 150
 
 func initialize(_target):
 	target = _target
 	velocity = target * SPEED
 	
 func _physics_process(delta):
-	if SPEED > 0:
-		move_and_slide()
-		SPEED -= 10 * delta
-		if SPEED <= 0:
-			SPEED = 0
+	if absorved:
+		position = position.move_toward(player.position, delta * speed)
+		speed += 1000 * delta
+	else:
+		if SPEED > 0:
+			move_and_slide()
+			SPEED -= 10 * delta
+			if SPEED <= 0:
+				SPEED = 0
 
 func _on_area_area_entered(area):
 	if !taken and area and area.is_in_group("players"):
@@ -24,3 +31,9 @@ func _on_area_area_entered(area):
 func _on_animation_player_animation_finished(anim_name):
 	visible = false
 	queue_free()
+
+
+func _on_magnet_area_entered(area):
+	if area and area.is_in_group("players"):
+		absorved = true
+		player = area
